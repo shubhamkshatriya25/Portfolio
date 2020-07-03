3ed92b5f2b8b18333dbbff2e4174ba42b7e50d0c
Shubham Kshatriya <shubhamkshatriya521@gmail.com>
2020-07-03 23:05:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,22 +125,6 @@
    });
  });
 
- $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
 
 })(jQuery);
 
